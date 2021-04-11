b03b3fc2c591f042affc2487be0a5af8e9db7698
Thiago Veronezi <thiago@veronezi.org>
2021-04-11 09:39:39 -0400

    preparing world update

--- main.lua
+++ main.lua
@@ -2,11 +2,15 @@ local World = require("world")
 local world = World:new()
 
 function love.load()
-    
+
 end
 
 function love.update(dt)
-
+    world:update_mouse_cell(
+        love.mouse.getX(),
+        love.mouse.getY()
+    )
+    world:update_simulation()
 end
 
 function love.draw()
@@ -15,4 +19,16 @@ end
 
 function love.mousepressed(x, y, button, istouch)
     world:toggle_cell_at(x, y)
- end
+end
+
+function love.keypressed(key)
+    if key == "escape" then
+        world:reset()
+        return
+    end
+    if key == "space" then
+        world:toggle_simulation()
+        return
+    end
+
+end
--- world.lua
+++ world.lua
@@ -3,7 +3,12 @@ local Cell = require("cell")
 local refreshing = false
 
 local World = {
+    run_simulation = false,
     cell_size = 30,
+    mouse_cell = {
+        x = 0,
+        y = 0
+    },
     cells = {},
 }
 
@@ -14,12 +19,25 @@ function World:new(o)
     return o
 end
 
+function World.get_cell_coords_at(self, window_x, window_y)
+    local cell_x = math.floor(window_x / self.cell_size);
+    local cell_y = math.floor(window_y / self.cell_size);
+    return cell_x, cell_y
+end
+
+function World.update_mouse_cell(self, window_x, window_y)
+    local cell_x, cell_y = self:get_cell_coords_at(window_x, window_y)
+    self.mouse_cell = {
+        x = cell_x,
+        y = cell_y
+    }
+end
+
 function World.toggle_cell_at(self, window_x, window_y)
     if refreshing then 
         return 
     end
-    local cell_x = math.floor(window_x / self.cell_size);
-    local cell_y = math.floor(window_y / self.cell_size);
+    local cell_x, cell_y = self:get_cell_coords_at(window_x, window_y)
     print("toggle_cell_at -> x: ".. window_x .. "; y: " .. window_y)
     print("                  cell_x: ".. cell_x .. "; cell_y: " .. cell_y)
     local cell_key = cell_x .. ":" ..cell_y
@@ -50,7 +68,25 @@ function World.draw(self)
             self.cell_size
         )
     end
+    love.graphics.rectangle("fill", 
+        self.mouse_cell.x * self.cell_size, 
+        self.mouse_cell.y * self.cell_size, 
+        self.cell_size,
+        self.cell_size
+    )
     refreshing = false
+end
+
+function World.reset(self)
+    self.cells = {}
+end
+
+function World.toggle_simulation(self) 
+    self.run_simulation = not self.run_simulation
+end
+
+function World.update_simulation(self)
+
 end
 
 return World
